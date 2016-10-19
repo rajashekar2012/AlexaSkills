becab5077a2d8078e98be7ae1a2e725adc73d158
rajashekar <rajashekarkhallel@gmail.com>
2016-10-19 19:52:10 +0530

    added data obj

--- index.js
+++ index.js
@@ -4,6 +4,23 @@ module.change_code = 1;
 var alexa = require( 'alexa-app' );
 var app = new alexa.app( 'test-skill' );
 
+var buddy={
+marks:{
+	john:{
+		name:"john",
+		percent:82,
+		math:95,
+		physics:88,
+		english:86
+	},
+	linda:{
+		name:"linda",
+		percent:65,
+		math:69,
+		physics:70,
+		english:66 }
+	}
+};
 
 app.launch( function( request, response ) {
 	response.say( 'Welcome to your test skill' ).reprompt( 'Way to go. You got it to run. Bad ass.' ).shouldEndSession( false );
@@ -19,15 +36,28 @@ app.error = function( exception, request, response ) {
 
 app.intent('getMarks',
   {
-    "slots":{"studentName":"LITERAL"},
+    "slots":{"studentName":"LITERAL","subject":"LITERAL"},
     "utterances":[ 
 		"how much did {john|studentName} scored",
 		"give me the test score of {john|studentName}",
-		"tell me how much did {john|studentName} scored"]
+		"tell me how much did {john|studentName} scored",
+		
+		"how much did {john|studentName} scored in {physics|subject} ",
+		"tell me how much did {john|studentName} scored in {physics|subject}"
+		]
   },
   function(request,response) {
-   // var studentName = request.slot('studentName');
-    response.say("john has scored 82 percent");
+    var studentName = request.slot('studentName');
+    var subject=request.slot('subject');
+
+    if(subject)
+    {
+    	response.say(studentName+" has scored "+ buddy[studentName][subject] +" in "+subject);
+    }
+    else
+    {
+    	response.say(studentName+" has scored "+ buddy[studentName]["percent"] +" percent");
+    }
   }
 );
 
